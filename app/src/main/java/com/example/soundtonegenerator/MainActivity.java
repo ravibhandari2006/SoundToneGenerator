0945c8ac0b1d92a3a32d176490e4f23d6a6b18a4
ravib <ravibhandari2006@gmail.com>
2022-07-14 12:07:50 +0530

    Latest

--- app/src/main/java/com/example/soundtonegenerator/MainActivity.java
+++ app/src/main/java/com/example/soundtonegenerator/MainActivity.java
@@ -9,7 +9,6 @@ import android.media.AudioTrack;
 import android.os.Build;
 import android.os.Handler;
 import android.support.annotation.RequiresApi;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -22,11 +21,12 @@ import java.util.Set;
 public class MainActivity extends Activity implements View.OnClickListener{
 
     private final int duration = 3; // seconds
-    private final int sampleRate = 8000;
+    private final int sampleRate = 44100;
     private final int numSamples = duration * sampleRate;
     private final double sample[] = new double[numSamples];
     private final double freqOfTone = 1000; // hz
     private final byte generatedSnd[] = new byte[2 * numSamples];
+    private final byte StereoSnd[] = new byte[4 * numSamples];
     private AudioManager aMgr;
     Handler handler = new Handler();
     Button btngentone;
@@ -72,10 +72,18 @@ public class MainActivity extends Activity implements View.OnClickListener{
     @RequiresApi(api = Build.VERSION_CODES.P)
     public float audioStreamVol(Context context){
         aMgr = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
+        int maxvol = aMgr.getStreamMaxVolume(aMgr.STREAM_MUSIC);
+        int minvol = aMgr.getStreamMinVolume(aMgr.STREAM_MUSIC);
+        Log.i(TAG,"Max_vol = "+Integer.toString(maxvol));
+        Log.i(TAG,"Min_vol = "+Integer.toString(minvol));
         int curType = (int) aMgr.STREAM_MUSIC;
         int vol = aMgr.getStreamVolume(curType);
+        int level = (maxvol-minvol)/2;
+        aMgr.setStreamVolume(curType,level,0);
+        Log.i(TAG,"Curr_vol = "+Integer.toString(vol));
         int deviceType = findDeviceType();
         float volumedB = aMgr.getStreamVolumeDb(curType, vol, deviceType);
+        //float volumedB = aMgr.getStreamVolumeDb(curType, 3, deviceType);
         return volumedB;
     }
 
@@ -121,22 +129,33 @@ public class MainActivity extends Activity implements View.OnClickListener{
         // convert to 16 bit pcm sound array
         // assumes the sample buffer is normalised.
         int idx = 0;
+        int flag =0;
         for (final double dVal : sample) {
             // scale to maximum amplitude
             final short val = (short) ((dVal * 32767));
             // in 16 bit wav PCM, first byte is the low order byte
-            generatedSnd[idx++] = (byte) (val & 0x00ff);
-            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
-
+//                generatedSnd[idx++] = (byte) (val & 0x00ff);
+//                generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+//            //Left ear
+//            StereoSnd[idx++] = (byte) (val & 0x00ff);
+//            StereoSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+//            StereoSnd[idx++] = (byte) (0);
+//            StereoSnd[idx++] = (byte) (0);
+            //Right ear
+            StereoSnd[idx++] = (byte) (0);
+            StereoSnd[idx++] = (byte) (0);
+            StereoSnd[idx++] = (byte) (val & 0x00ff);
+            StereoSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
         }
     }
 
     void playSound(){
         final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
-                sampleRate, AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
+                sampleRate, AudioFormat.CHANNEL_OUT_STEREO,
+                AudioFormat.ENCODING_PCM_16BIT, StereoSnd.length,
                 AudioTrack.MODE_STATIC);
-        audioTrack.write(generatedSnd, 0, generatedSnd.length);
+//        audioTrack.write(generatedSnd, 0, generatedSnd.length);
+        audioTrack.write(StereoSnd, 0, StereoSnd.length);
         audioTrack.play();
     }
 }
